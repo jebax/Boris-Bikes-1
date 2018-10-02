22a89b64730357f91b606aa5a57655f29a92fd24
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-02 15:11:32 +0100

    release_bike return a Bike object

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,7 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
+  it "should return a Bike" do
+    expect(DockingStation.new.release_bike).to be_an_instance_of(Bike)
+  end
 end
