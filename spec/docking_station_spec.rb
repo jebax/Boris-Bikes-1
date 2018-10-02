d087fbd7d01dbd9f50c9f77fbda446d063689af8
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-02 14:33:41 +0100

    Instaces of DockingStation respond to release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,9 @@
 require "docking_station"
 
 describe DockingStation do
+  it "should respond to 'release_bike'" do
+    obj = DockingStation.new
+    expect(obj).to respond_to(:release_bike)
+  end
 
 end
