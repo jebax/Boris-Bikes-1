720bfdf42ba8d86a06740ce7f88388c922757b93
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-02 14:37:55 +0100

    Refactored respond to release_bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,6 @@
 require "docking_station"
 
 describe DockingStation do
-  it "should respond to 'release_bike'" do
-    obj = DockingStation.new
-    expect(obj).to respond_to(:release_bike)
-  end
+  it { is_expected.to respond_to :release_bike}
 
 end
