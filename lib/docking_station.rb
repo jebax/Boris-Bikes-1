9266eabe0f623ddfc4272940ea40afe3778363e3
Anouska Hopkins <anouskahopkins@gmail.com>
2018-10-03 16:30:27 +0100

    Refactored using srp

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,15 +9,25 @@ attr_reader :docked_bikes
   end
 
   def release_bike
-    raise "Docking station is empty" if @docked_bikes.empty?
+    raise "Docking station is empty" if empty?
 
     @docked_bikes.shift
   end
 
   def dock(bike)
-    raise "Docking station is full" if @docked_bikes.length == 20
+    raise "Docking station is full" if full?
 
     @docked_bikes << bike
+  end
+
+  private
+
+  def full?
+    return true if @docked_bikes.length == 20
+  end
+
+  def empty?
+    return true if @docked_bikes.empty?
   end
 
 end
