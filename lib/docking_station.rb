a81878b9f0615fa81a95421933789392d78134ac
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-03 15:01:38 +0100

    raise error for empty docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,7 +4,13 @@ class DockingStation
 
 attr_reader :docked_bike
 
+  def initialize
+    @docked_bike
+  end
+
   def release_bike
+    raise "Docking station is empty" if @docked_bike.nil?
+    
     Bike.new
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,10 +4,12 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike}
 
   it "should return a Bike" do
+    subject.dock(Bike.new)
     expect(subject.release_bike).to be_an_instance_of(Bike)
   end
 
   it "should be working " do
+    subject.dock(Bike.new)
     expect(subject.release_bike.working?).to eq true
   end
 
@@ -19,6 +21,10 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.docked_bike).to eq bike
+  end
+
+  it "should raise an error" do
+    expect {subject.release_bike}.to raise_error("Docking station is empty")
   end
 
 end
