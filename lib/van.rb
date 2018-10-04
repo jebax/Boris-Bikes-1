2409ea9aa96f5757802aaaa99299d9f845907b18
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-04 17:06:19 +0100

    allowed for delivering working bikes to station

--- lib/van.rb
+++ lib/van.rb
@@ -5,4 +5,21 @@ class Van
     @storage = []
   end
 
+  def take_working_bikes(garage)
+    raise "Van not empty" unless @storage.empty?
+    garage.storage.each do |bike|
+      @storage << bike if bike.working?
+      garage.storage.delete(bike)
+    end
+  end
+
+  def release_working_bikes(station)
+    @storage.each do |bike|
+      if bike.working?
+        station.docked_bikes << bike
+        @storage.delete(bike)
+      end
+    end
+  end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,8 +3,40 @@ require 'van'
 describe Van do
   let(:station) { double(:station) }
   let(:bike) { double(:bike) }
+  let(:garage) { double(:garage) }
 
   it 'should be able to store bikes' do
     expect(subject.storage).to eq []
+  end
+
+  describe "#take_working_bikes" do
+    it "should take bikes from a garage" do
+      allow(garage).to receive(:storage).and_return [bike]
+      allow(bike).to receive(:working?).and_return true
+      subject.take_working_bikes(garage)
+      expect(subject.storage).to eq [bike]
+    end
+
+    it "should not take a broken bike" do
+      allow(garage).to receive(:storage).and_return [bike]
+      allow(bike).to receive(:working?).and_return false
+      subject.take_working_bikes(garage)
+      expect(subject.storage).to eq []
+    end
+
+    it "should not take bikes unless empty" do
+      subject.storage << bike
+      expect {subject.take_working_bikes(garage)}.to raise_error "Van not empty"
+    end
+  end
+
+  describe "#release_working_bikes" do
+    it "should release a working bike to a station" do
+      allow(bike).to receive(:working?).and_return true
+      allow(station).to receive(:docked_bikes).and_return []
+      subject.storage << bike
+      subject.release_working_bikes(station)
+      expect(station.docked_bikes).to eq [bike]
+    end
   end
 end
